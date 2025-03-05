a5aeec0483ad018e6aede49866fdd1909095b710
Brandon Johnson <johnsbr99@gmail.com>
2025-03-05 07:57:21 -0700

    Added logic to update captions dynamically and for fetching img urls

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,29 @@
+import { useState, useEffect} from 'react'
+
 export default function Main() {
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg" 
+    })
+
+    const [allMemes, setAllMemes] = useState([])
+    
+    useEffect(() => {
+        fetch('https://api.imgflip.com/get_memes')
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    }, [])
+    
+    console.log(allMemes)
+
+    function handleChange(event) {
+        const {value, name} = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
 
     return (
         <main>
@@ -8,6 +33,8 @@ export default function Main() {
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
+                        value={meme.topText}
+                        onChange={handleChange}
                     />
                 </label>
 
@@ -16,14 +43,16 @@ export default function Main() {
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
+                        value={meme.bottomText}
+                        onChange={handleChange}
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main>
     )
